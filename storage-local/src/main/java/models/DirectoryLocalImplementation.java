5e98ccda0301ad189e2ac9176b80176bd760f957
Marko <Marko@DESKTOP-FFVEM1G>
2019-10-25 18:31:16 +0200

    Maven project and classes that implements interfaces from specification project created...

---
+++ storage-local/src/main/java/models/DirectoryLocalImplementation.java
@@ -0,0 +1,49 @@
+package models;
+
+import specs.Directory;
+
+public class DirectoryLocalImplementation implements Directory {
+
+	@Override
+	public void createDirectory(String name, String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteDirectory(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uploadDirectory(String selectedPath, String destinationPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void downloadDirectory(String selectedPath, String destinationPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listAllFiles(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listFiles(String path, String extension) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void listDirectories(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ storage-local/src/main/java/models/FileLocalImplementation.java
@@ -0,0 +1,31 @@
+package models;
+
+import specs.File;
+
+public class FileLocalImplementation implements File {
+
+	@Override
+	public void createFile(String name, String path, String fileExtension) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteFile(String path) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void uploadFile(String selectedPath, String destinationPath, String fileExtension) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void downloadFile(String selectedPath, String destinationPath) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
