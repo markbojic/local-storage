83d70ca4542ea6e3a42e14a7a7c37e147e4432b9
Marko <Marko@DESKTOP-FFVEM1G>
2019-10-28 19:17:27 +0100

    Create, Delete, Up/down directory implemented

--- storage-local/src/main/java/main/Main.java
+++ storage-local/src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import models.DirectoryLocalImplementation;
 import models.FileLocalImplementation;
 
 public class Main {
@@ -10,14 +11,25 @@ public class Main {
 		FileLocalImplementation localFile = new FileLocalImplementation();
 		
 		// Create test
-		localFile.createFile("TestFajl.txt", "C:\\Users\\R930\\Desktop");
+		//localFile.createFile("TestFajl.txt", "C:\\Users\\R930\\Desktop");
 		// Upload test
-		localFile.uploadFile("C:\\Users\\R930\\Desktop\\TestFajl.txt", "C:\\Users\\R930\\Desktop\\Test Folder");
+		//localFile.uploadFile("C:\\Users\\R930\\Desktop\\TestFajl.txt", "C:\\Users\\R930\\Desktop\\Test Folder");
 		// Delete test
-		localFile.deleteFile("C:\\Users\\R930\\Desktop\\TestFajl.txt");
+		//localFile.deleteFile("C:\\Users\\R930\\Desktop\\TestFajl.txt");
 		// Download test
-		localFile.downloadFile("C:\\Users\\R930\\Desktop\\Test Folder\\TestFajl.txt", "C:\\Users\\R930\\Desktop");
+		//localFile.downloadFile("C:\\Users\\R930\\Desktop\\Test Folder\\TestFajl.txt", "C:\\Users\\R930\\Desktop");
 		// zip test dodaj iznad da prvo zipuje pa download zipovano
+		
+		DirectoryLocalImplementation localDir = new DirectoryLocalImplementation();
+		
+		// Create test
+		localDir.createDirectory("TestDir", "C:\\Users\\R930\\Desktop");
+		// Upload test
+		localDir.uploadDirectory("C:\\Users\\R930\\Desktop\\TestDir", "C:\\Users\\R930\\Desktop\\Test Folder");
+		// Delete test
+		localDir.deleteDirectory("C:\\Users\\R930\\Desktop\\TestDir");
+		// Download test
+		localDir.downloadDirectory("C:\\Users\\R930\\Desktop\\Test Folder\\TestDir", "C:\\Users\\R930\\Desktop");
 	}
 
 }
--- storage-local/src/main/java/models/DirectoryLocalImplementation.java
+++ storage-local/src/main/java/models/DirectoryLocalImplementation.java
@@ -1,31 +1,116 @@
 package models;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import specs.DirectoryManipulation;
 
 public class DirectoryLocalImplementation implements DirectoryManipulation {
 
 	@Override
 	public void createDirectory(String name, String path) {
-		// TODO Auto-generated method stub
+		Path destPath;
 		
+		if (path == null || path.equals("")) {
+			System.out.println("The path is not valid!");
+		}
+		else {
+			destPath = Paths.get(path);
+			
+			if (name != null && !name.equals("")) {
+				if(Files.exists(destPath) && !Files.exists(Paths.get(destPath + File.separator + name))) {
+					try {
+						Files.createDirectory(Paths.get(destPath + File.separator + name));
+						System.out.println("Directory " + name + " created successfully!");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				} else {
+					System.out.println("Directory under given name already exists...");
+				}
+			}
+			else {
+				System.out.println("Name is not valid!");
+			}
+		}
 	}
 
 	@Override
 	public void deleteDirectory(String path) {
-		// TODO Auto-generated method stub
+		Path dirPath;
+		
+		if (path == null || path.equals("")) {
+			System.out.println("The path is not valid!");
+		}
+		else {
+			dirPath = Paths.get(path);
+
+			try {
+				Files.deleteIfExists(dirPath);
+				System.out.println("Directory " + dirPath.getFileName() + " deleted!");
+			} catch (IOException e) {
+				System.out.println("Fail...");
+				e.printStackTrace();
+			}
+		}
 		
 	}
 
 	@Override
 	public void uploadDirectory(String selectedPath, String destinationPath) {
-		// TODO Auto-generated method stub
+		Path oldPath, newPath;
 		
+		if (selectedPath == null || selectedPath.equals("") || destinationPath == null || destinationPath.equals("")) {
+			System.out.println("The path is not valid!");
+		}
+		else {
+			oldPath = Paths.get(selectedPath);
+			newPath = Paths.get(destinationPath);
+			String name = selectedPath.substring(selectedPath.lastIndexOf(File.separator) + 1);
+			//System.out.println("Directory name: " + name);
+			if (Files.exists(oldPath) && Files.exists(newPath) && !Files.exists(Paths.get(newPath + File.separator + name))) {
+				try {
+					Files.copy(oldPath, Paths.get(newPath + File.separator + name));
+					System.out.println("Directory " + name + " uploaded to " + newPath);
+				} catch (IOException e) {
+					System.out.println("Failed to upload...");
+					e.printStackTrace();
+				}
+			}
+			else {
+				System.out.println("Error!");
+			}
+		}
 	}
 
 	@Override
 	public void downloadDirectory(String selectedPath, String destinationPath) {
-		// TODO Auto-generated method stub
+		Path oldPath, newPath;
 		
+		if (selectedPath == null || selectedPath.equals("") || destinationPath == null || destinationPath.equals("")) {
+			System.out.println("The path is not valid!");
+		}
+		else {
+			oldPath = Paths.get(selectedPath);
+			newPath = Paths.get(destinationPath);
+			String name = selectedPath.substring(selectedPath.lastIndexOf(File.separator) + 1);
+			//System.out.println("Directory name: " + name);
+			if (Files.exists(oldPath) && Files.exists(newPath) && !Files.exists(Paths.get(newPath + File.separator + name))) {
+				try {
+					Files.copy(oldPath, Paths.get(newPath + File.separator + name));
+					System.out.println("Directory " + name + " downloaded to " + newPath);
+				} catch (IOException e) {
+					System.out.println("Failed to download...");
+					e.printStackTrace();
+				}
+			}
+			else {
+				System.out.println("Error!");
+			}
+		}
 	}
 
 	@Override
